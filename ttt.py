547b2a76bbcdfafc4cc7473cb9a725a1f4eb30eb
Sunil Jacob <sunil1405@gmail.com>
2018-02-12 07:43:23 +0530

    Create ttt.py

---
+++ ttt.py
@@ -0,0 +1,11 @@
+from IPython.display import clear_output
+def drawBoard(board):
+    #global s
+    #s=board
+    #clear_output()
+    print(' ' + board[1] + ' | ' + board[2] +' | ' + board[3])
+    print('-----------')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('-----------')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    #return s
